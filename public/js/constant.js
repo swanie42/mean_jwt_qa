14f40a5ca095410f0999201ea46bce8025493a98
Jeff <swanie42@gmail.com>
2017-01-24 10:15:15 -0700

    change of name

--- public/js/constant.js
+++ public/js/constant.js
@@ -1,6 +1,6 @@
-/*var appConstants=angular.module('app.constants',[])
-                        .constant('CONSTANT',{"API_URL":"http://www.linkository.com"});
-*/
-
 var appConstants=angular.module('app.constants',[])
-                        .constant('CONSTANT',{"API_URL":"http://localhost:8080"});
+                        .constant('CONSTANT',{"API_URL":"http://138.197.215.137"});
+
+
+// var appConstants=angular.module('app.constants',[])
+//                         .constant('CONSTANT',{"API_URL":"http://localhost:8080"});
